403cce12c9293df314941a34437e5977cb3b99d2
sanemat <o.gata.ken@gmail.com>
2013-09-02 02:20:30 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in bundler-update_stdout.gemspec
+gemspec
---
+++ lib/bundler/update_stdout.rb
@@ -0,0 +1,7 @@
+require "bundler/update_stdout/version"
+
+module Bundler
+  module UpdateStdout
+    # Your code goes here...
+  end
+end
