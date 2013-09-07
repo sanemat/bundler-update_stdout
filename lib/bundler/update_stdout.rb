b7c1f658c88c5a56648e668676c13e48b1338357
sanemat <o.gata.ken@gmail.com>
2013-09-08 03:52:53 +0900

    Use bundler itself

--- lib/bundler/update_stdout.rb
+++ lib/bundler/update_stdout.rb
@@ -1,11 +1,16 @@
 require "bundler/update_stdout/version"
+require 'bundler/cli'
 
 module Bundler
-  module UpdateStdout
-    class Base
-      def self.update_stdout
-        puts 'bundle-update_stdout'
-      end
+  class CLI
+    desc "update_stdout", "update the current environment into stdout"
+    long_desc <<-D
+      Update will install the newest versions of the gems listed in the Gemfile. Use
+      update when you have changed the Gemfile, or if you want to get the newest
+      possible versions of the gems in the bundle.
+    D
+    def update_stdout(*gems)
+      puts 'bundle-update_stdout'
     end
   end
 end
