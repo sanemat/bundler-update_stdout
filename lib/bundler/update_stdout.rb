2fceb766f745d537f979087a0379162302225547
sanemat <o.gata.ken@gmail.com>
2013-09-08 10:10:15 +0900

    Use single quote by rubocop

--- lib/bundler/update_stdout.rb
+++ lib/bundler/update_stdout.rb
@@ -1,4 +1,4 @@
-require "bundler/update_stdout/version"
+require 'bundler/update_stdout/version'
 require 'bundler/cli'
 
 # OVERRIDE
@@ -20,20 +20,20 @@ end
 
 module Bundler
   class CLI
-    desc "update_stdout", "update the current environment into stdout"
+    desc 'update_stdout', 'update the current environment into stdout'
     long_desc <<-D
       Update will install the newest versions of the gems listed in the Gemfile. Use
       update when you have changed the Gemfile, or if you want to get the newest
       possible versions of the gems in the bundle.
     D
-    method_option "source", :type => :array, :banner => "Update a specific source (and all gems associated with it)"
-    method_option "local", :type => :boolean, :banner =>
-      "Do not attempt to fetch gems remotely and use the gem cache instead"
+    method_option 'source', :type => :array, :banner => 'Update a specific source (and all gems associated with it)'
+    method_option 'local', :type => :boolean, :banner =>
+      'Do not attempt to fetch gems remotely and use the gem cache instead'
     # OVERRIDE
-    method_option "quiet", :type => :boolean, :default => true, :banner =>
-      "Only output warnings and errors."
-    method_option "full-index", :type => :boolean, :banner =>
-        "Use the rubygems modern index instead of the API endpoint"
+    method_option 'quiet', :type => :boolean, :default => true, :banner =>
+      'Only output warnings and errors.'
+    method_option 'full-index', :type => :boolean, :banner =>
+        'Use the rubygems modern index instead of the API endpoint'
     def update_stdout(*gems)
       update(*gems)
     end
