f23923811f59216006101e6cae113064c34260db
sanemat <o.gata.ken@gmail.com>
2013-09-08 10:06:22 +0900

    Use actual bundle update

--- lib/bundler/update_stdout.rb
+++ lib/bundler/update_stdout.rb
@@ -26,37 +26,16 @@ module Bundler
       update when you have changed the Gemfile, or if you want to get the newest
       possible versions of the gems in the bundle.
     D
+    method_option "source", :type => :array, :banner => "Update a specific source (and all gems associated with it)"
+    method_option "local", :type => :boolean, :banner =>
+      "Do not attempt to fetch gems remotely and use the gem cache instead"
+    # OVERRIDE
+    method_option "quiet", :type => :boolean, :default => true, :banner =>
+      "Only output warnings and errors."
+    method_option "full-index", :type => :boolean, :banner =>
+        "Use the rubygems modern index instead of the API endpoint"
     def update_stdout(*gems)
-
-      sources = Array(options[:source])
-      # OVERRIDE
-      Bundler.ui.level = "warn"
-
-      if gems.empty? && sources.empty?
-        # We're doing a full update
-        Bundler.definition(true)
-      else
-        # cycle through the requested gems, just to make sure they exist
-        names = Bundler.locked_gems.specs.map{ |s| s.name }
-        gems.each do |g|
-          next if names.include?(g)
-          raise GemNotFound, not_found_message(g, names)
-        end
-        Bundler.definition(:gems => gems, :sources => sources)
-      end
-
-      Bundler::Fetcher.disable_endpoint = options["full-index"]
-
-      opts = {"update" => true, "local" => options[:local]}
-      # rubygems plugins sometimes hook into the gem install process
-      Gem.load_env_plugins if Gem.respond_to?(:load_env_plugins)
-
-      Bundler.definition.validate_ruby!
-      Installer.install Bundler.root, Bundler.definition, opts
-      Bundler.load.cache if Bundler.root.join("vendor/cache").exist?
-      clean if Bundler.settings[:clean] && Bundler.settings[:path]
-      Bundler.ui.confirm "Your bundle is updated!"
-      Bundler.ui.confirm without_groups_message if Bundler.settings.without.any?
+      update(*gems)
     end
   end
 end
